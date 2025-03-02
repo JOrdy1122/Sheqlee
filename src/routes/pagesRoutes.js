ca28e55c16ec72718680ed4a5a3b2c8e92460f0c
jk <johansking959@gmail.com>
2025-03-02 13:50:27 +0300

    protect Middleware added to routes

--- src/routes/pagesRoutes.js
+++ src/routes/pagesRoutes.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const pageController = require('../controllers/pageController');
 const { protect } = require('./../middlewares/auth'); 
-
-const {getAllPages,getPage,updatePage,deletePage} = pageController;
+const {getAllPages,getPage,updatePage,deletePage,createPage} = pageController;
 const router = express.Router();
 
 
-router.post('/', createPage);
+router.post('/', protect, createPage);
 
-// Get all pages 
+
 router.get('/', getAllPages);
+
 
 router.get('/:id', getPage);
 
 
-router.put('/:id', protect,updatePage);
+router.put('/:id',protect, updatePage);
 
 
-router.delete('/:id',protect, deletePage);
+router.delete('/:id', protect,deletePage);
 
 module.exports = router;
